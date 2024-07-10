ffc161b461ba9045188d634684e6da980d8548c8
deeshubhatt07 <deeshubhatt07@gmail.com>
2024-07-11 00:23:08 +0530

    added inputs for 1st and 2nd player

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Game {
     public static void main(String args[]) {
-        System.out.println("Game initialized");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("1st player --------------------------------");
+        System.out.println("Enter the health for the first player");
+        int firstPlayerHealth = scanner.nextInt();
+        System.out.println("Enter the strength for the first player");
+        int firstPlayerStrength = scanner.nextInt();
+        System.out.println("Enter the Attack for the first player");
+        int firstPlayerAttack = scanner.nextInt();
+
+        System.out.println("2nd player --------------------------------");
+        System.out.println("Enter the health for the second player");
+        int secondPlayerHealth = scanner.nextInt();
+        System.out.println("Enter the strength for the second player");
+        int secondPlayerStrength = scanner.nextInt();
+        System.out.println("Enter the Attack for the second player");
+        int secondPlayerAttack = scanner.nextInt();
+        
     }
 }
